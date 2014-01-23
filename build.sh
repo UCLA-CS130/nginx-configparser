a8ec85e3bd413720d8e7eda88e7978c4912649d7
James Farrell <jfarrell468@gmail.com>
2014-01-22 18:58:38 -0800

    Fix a typo in the build script, and add an example config file

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 case `uname` in
   Linux) g++ config_parser.cc config_parser_main.cc -std=c++0x -g -Wall -o config_parser;;
-  Darwin) clang++ config_parser.cc config_parser_main.cc -std=c++11 -g -Wall
-    -stdlib=libc++ -o config_parser;;
+  Darwin) clang++ config_parser.cc config_parser_main.cc -std=c++11 -g -Wall -stdlib=libc++ -o config_parser;;
   *) echo "Unknown operating system";;
 esac
