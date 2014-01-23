bcc210030be0e554aab6013cdee3ff1e783f33d7
James Farrell <jfarrell468@gmail.com>
2014-01-22 18:53:54 -0800

    Add required files for googletest 1.7.0, a skeleton unit test, and a build script for the unit test.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 case `uname` in
-  Linux) g++ config_parser.cc config_parser_main.cc -std=c++0x -g -Wall;;
-  Darwin) clang++ config_parser.cc config_parser_main.cc -std=c++11 -g -Wall -stdlib=libc++;;
+  Linux) g++ config_parser.cc config_parser_main.cc -std=c++0x -g -Wall -o config_parser;;
+  Darwin) clang++ config_parser.cc config_parser_main.cc -std=c++11 -g -Wall
+    -stdlib=libc++ -o config_parser;;
   *) echo "Unknown operating system";;
 esac
---
+++ config_parser_test.cc
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(AdditionTest, OnePlusOne) {
+  EXPECT_EQ(2, 1 + 1);
+}
