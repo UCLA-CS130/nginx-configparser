d9c137ba512eb6cf03e6b6f1909c78b42e27832e
cacoderquan <xni@caltech.edu>
2015-04-15 15:14:51 -0700

    hw2 commit

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,7 +1,10 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <sstream>
+#include<string>
 
-TEST(NginxConfigParserTest, SimpleConfig) {
+
+TEST(NginxConfigParserTest, SimpleConfigFromFile) {
   NginxConfigParser parser;
   NginxConfig out_config;
 
@@ -9,3 +12,35 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+
+TEST(NginxConfigStatementTest, Tostring) {
+  NginxConfigStatement statement;
+  statement.tokens_push_back("foo");
+  statement.tokens_push_back("bar");
+  EXCEPT_EQ("foo bar;\n",statement.Tostring(0));
+}
+
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+	bool ParseString(const std::string& config_string) {
+		std::stringstream config_stream(config_string);
+		return parserr_Parse(&config_stream, &config_);
+	}
+
+	NiginxCOnfigParser parser_;
+	NiginxConfig config_;
+};
+
+TEST_F(NiginxStringConfigTest, SimpleTestConfig){
+	EXCEPT_FAULSE(ParseString("foo bar"));
+}
+
+TEST_F(NiginxStringConfigTest, NestedTestConfig){
+	EXCEPT_TRUE(ParseString(
+		"foo { "
+		"  bar { "
+		"     bax qux;"
+		"   }"
+		"}"));
+} 
