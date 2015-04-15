44a9ee6b5d09de2fee93e2478f680f0e82898cc2
helbaum <helbaum@caltech.edu>
2015-04-15 16:17:05 -0700

    New unit tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,7 +1,9 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <sstream>
+#include <string>
 
-TEST(NginxConfigParserTest, SimpleConfig) {
+TEST(NginxConfigParserTest, SimpleConfigFromFile) {
   NginxConfigParser parser;
   NginxConfig out_config;
 
@@ -9,3 +11,40 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigStatementTest, ToString) {
+    NginxConfigStatement statement;
+    statement.tokens_.push_back("foo");
+    statement.tokens_.push_back("bar");
+    EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+
+class NginxStringConfigTest : public ::testing::Test {
+    protected:
+        bool ParseString(const std::string& config_string) {
+            std::stringstream config_stream(config_string);
+            return parser_.Parse(&config_stream, &config_);
+        }
+
+        NginxConfigParser parser_;
+        NginxConfig config_;
+};
+
+
+TEST_F(NginxStringConfigTest, SimpleTextConfig) {
+    EXPECT_TRUE(ParseString("foo bar;"));
+    EXPECT_EQ(1, config_.statements_.size());
+}
+
+TEST_F(NginxStringConfigTest, SimpleBadTextConfig) {
+    EXPECT_FALSE(ParseString("foo bar"));
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig) {
+    EXPECT_TRUE(ParseString("foo { "
+                            "  bar { "
+                            "    baz fasls;"
+                            "  }"
+                            "}"));
+}
+                
