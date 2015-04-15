c07b0cf379e114365986a44695a666e2d22580a0
Justin Leong <justinleong16@gmail.com>
2015-04-14 21:17:51 -0700

    Added proxy_config and wrote test with it that failed.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -2,10 +2,25 @@
 #include "config_parser.h"
 
 TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+    NginxConfigParser parser;
+    NginxConfig out_config;
 
-  bool success = parser.Parse("example_config", &out_config);
+    bool success = parser.Parse("example_config", &out_config);
 
-  EXPECT_TRUE(success);
+    EXPECT_TRUE(success);
+}
+
+
+/*
+ * proxy.config based on example from this website:
+ * http://nginx.org/en/docs/beginners_guide.html#proxy
+ */
+
+TEST(NginxConfigParserTest, ProxyConfig) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("proxy_config", &out_config);
+
+    EXPECT_TRUE(success);
 }
