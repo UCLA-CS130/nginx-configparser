74b3bbf577768b5db8479a6746039f5107960058
Eric Gorlin <ericgorlin@gmail.com>
2015-04-17 01:59:36 -0700

    Added more tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -61,3 +61,48 @@ TEST_F(NginxStringConfigTest, BadNestedConfig) {
         "}"
         "}"));
 }
+
+
+TEST_F(NginxStringConfigTest, BadNestedConfig2) {
+    EXPECT_FALSE(ParseString(
+        "{ test1"
+        "    { this is a test} "
+        "}}"));
+}
+
+TEST(NginxConfigParserTest, ToString2) {
+    NginxConfigStatement statement;
+    statement.tokens_.push_back("foo");
+    statement.tokens_.push_back("{");
+    statement.tokens_.push_back("bar");
+    statement.tokens_.push_back("}");
+    EXPECT_EQ("foo { bar };\n", statement.ToString(0));
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig2) {
+    EXPECT_TRUE(ParseString(
+        "test1 { "
+        "   test2 test3 { test4"
+        "      test5;"
+        "   }"
+        "}"));
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig3) {
+    EXPECT_TRUE(ParseString(
+        "test1 { "
+        "   test2 {"
+        "      test3 {"
+        "         test4 {"
+        "            test5 {"
+        "               test6 {"
+        "                  test7 {"
+        "                     test8;"
+        "                  }"
+        "               }"
+        "            }"
+        "         }"
+        "      }"
+        "   }"
+        "}"));
+}
