7691f66757d477b5d91a8d741afad528873c85e9
dkawashi <dkawashi@caltech.edu>
2016-04-02 09:45:13 -0700

    Removed commented-out code

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -42,8 +42,6 @@ TEST(NginxConfigParserTest, FastCGIConfig) {
 TEST_F(NginxStringConfigTest, CurlyBracesConfig) {
 
   bool success = ParseString("foo { bar; }");
-  // EXPECT_EQ("expected", actual) << message
-  // To STOP immediately, write an ASSERT
   EXPECT_TRUE(success) << "Error with curly braces";
 }
 
@@ -52,30 +50,13 @@ TEST_F(NginxStringConfigTest, SimpleConfig){
 	bool success1 = ParseString("foo bar;");
 
 	EXPECT_TRUE(success1) << "one-line statement did not parse";
-	//EXPECT_EQ(1, config_.statements_.size()) << "More than 1 statement";
-	//EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0)) << "foo is not first statement";
 }
-
-//TEST(NginxConfigParserTest, MultiStatementConfig){
-	//std::stringstream config_stream("foo bar");
-	//NginxConfigParser parser1;
-	//NginxConfig out_config1;
-	//std::stringstream config_stream3("server { listen   80; server_name foo.com; root /home/ubuntu/sites/foo/; }");
-
-	//bool success1 = parser1.Parse(&config_stream3, &out_config1);
-	//EXPECT_TRUE(success1) << "Statement with 3 ; and curly braces did not parse";
-	//EXPECT_EQ(1, config_.statements_.size()) << "More than 1 statement";
-	//EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0)) << "foo is not first statement";
-//}
 
 TEST_F(NginxStringConfigTest, TripleBracesConfig){
 	bool success1 = ParseString("foo { barbaz; true { fun; } more;}}");
 	EXPECT_TRUE(success1) << "Statement triple curly braces did not parse";
-	//EXPECT_EQ(1, config_.statements_.size()) << "More than 1 statement";
-	//EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0)) << "foo is not first statement";
 }
-/* What do we test? 
-	nested block of statements (curly braces) */
+
 TEST(NginxConfigParserTest, ParserConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
