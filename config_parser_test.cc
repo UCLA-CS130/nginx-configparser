63c4670ebd0024aa67caa964f54a012dcb535312
Loko <lokokung@gmail.com>
2016-04-07 15:38:24 -0700

    Implemented passing tests for NginxConfig class

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -37,5 +37,37 @@ TEST_F(NginxConfigStatementTest, ToStringChildBlock) {
 	    parentStatement_.ToString(0));
 }
 
+/*****************************************************************
+ * Testing Class: NginxConfig
+ * Number of Tests: 2
+ *****************************************************************/
+class NginxConfigTest : public ::testing::Test {
+protected:
+  void addStatement(std::vector<std::string> statement) {
+    NginxConfigStatement* newState = new NginxConfigStatement;
+    for (unsigned int i = 0; i < statement.size(); ++i) {
+      newState->tokens_.push_back(statement[i]);
+    }
+    config_.statements_.emplace_back(newState);
+  }
+  NginxConfig config_;
+};
+
+TEST_F(NginxConfigTest, ToStringSingleStatement) {
+  std::vector<std::string> s1 {"foo", "bar"};
+  addStatement(s1);
+  EXPECT_EQ("foo bar;\n", config_.ToString(0));
+}
+
+TEST_F(NginxConfigTest, ToStringMultipleStatements) {
+  std::vector<std::string> s1 {"port", "8000"};
+  std::vector<std::string> s2 {"server", "google.com"};
+  std::vector<std::string> s3 {"root", "/home/"};
+  addStatement(s1);
+  addStatement(s2);
+  addStatement(s3);
+  EXPECT_EQ("port 8000;\nserver google.com;\nroot /home/;\n",
+	    config_.ToString(0));
+}
 
 
