2bf1fe0c7bc8c35e9960caf609762de6dc6f40ba
Esther Du <estherdu@gmail.com>
2016-04-07 16:57:29 -0700

    Added unit tests and fixed nesting bug.

--- config_parser.cc
+++ config_parser.cc
@@ -152,6 +152,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
   config_stack.push(config);
   TokenType last_token_type = TOKEN_TYPE_START;
   TokenType token_type;
+  int loop_num = 0;
   while (true) {
     std::string token;
     token_type = ParseToken(config_file, &token);
@@ -198,10 +199,16 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
       config_stack.top()->statements_.back().get()->child_block_.reset(
           new_config);
       config_stack.push(new_config);
+      loop_num += 1;
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END) {
+      if (last_token_type != TOKEN_TYPE_STATEMENT_END && loop_num < 1) {
         // Error.
         break;
+      }
+      else {
+        loop_num -= 1;
+        config_stack.top()->statements_.back().get()->tokens_.push_back(
+            token);
       }
       config_stack.pop();
     } else if (token_type == TOKEN_TYPE_EOF) {
@@ -209,6 +216,10 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
           last_token_type != TOKEN_TYPE_END_BLOCK) {
         // Error.
         break;
+      }
+      if (loop_num > 0) {
+        printf ("Nested loops brackets don't match.\n");
+        return false;
       }
       return true;
     } else {
--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
+#include <sstream>
+#include <string>
+
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
@@ -9,3 +12,53 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigParserTest, ToStringSimple) {
+	NginxConfigStatement statement;
+	statement.tokens_.push_back("foo");
+	statement.tokens_.push_back("bar");
+	EXPECT_EQ("foo bar;\n", statement.ToString(0)) << "foo bar wasn't serialized correctly";
+}
+
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+	bool ParseString(const std::string& config_string) {
+		std::stringstream config_stream(config_string);
+		return parser_.Parse(&config_stream, &config_);
+	}
+	NginxConfigParser parser_;
+	NginxConfig config_;
+};
+
+TEST_F(NginxStringConfigTest, SimpleConfig2) {
+	EXPECT_TRUE(ParseString("foo bar;")) << "Didn't parse successfully";
+	EXPECT_EQ(1, config_.statements_.size()) << "Didn't get one statement.";
+	EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0));
+}
+
+TEST_F(NginxStringConfigTest, EmptyConfig) {
+	EXPECT_FALSE(ParseString("")) << "Got empty string wrong";
+}
+
+TEST_F(NginxStringConfigTest, BadConfig) {
+	EXPECT_FALSE(ParseString("foo bar")) << "Bad config was parsed successfully";
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig) {
+	EXPECT_TRUE(ParseString("server { listen 80; }")) << "Nested config parsed incorrectly.";
+}
+
+TEST_F(NginxStringConfigTest, DoubleNestedConfig) {
+	// bug in code found and fixed
+	EXPECT_TRUE(ParseString("foo { bar { baz aux; } }")) << "Double nested config parsed incorrectly.";
+}
+
+TEST_F(NginxStringConfigTest, WrongDoubleNestedConfig) {
+	EXPECT_FALSE(ParseString("foo { bar { baz aux; }")) << "Wrong double nested config parsed successfully.";
+}
+
+TEST_F(NginxStringConfigTest, WrongDoubleNestedConfig2) {
+	EXPECT_FALSE(ParseString("foo { bar baz aux; } }")) << "Second wrong double nested config parsed successfully.";
+}
+
+
