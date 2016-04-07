e9c03c5f80f168a84f1b9a8aeb42cf51309651b3
Wade Hann-Caruthers <whanncar@quarterstaff.cms.caltech.edu>
2016-04-07 10:00:34 -0700

    I am committing this just so that we can make sure we can compare our unit tests.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,37 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+class NginxStringConfigTest : public :: testing :: Test {
+
+  protected:
+    bool ParseString(const std::string& config_string){
+
+      std::stringstream config_stream(config_string);
+      return parser_.Parse(&config_stream, &config_);
+
+    }
+
+    NginxConfigParser parser_;
+    NginxConfig config_;
+
+
+};
+
+TEST_F(NginxStringConfigTest, SimpleConfig){
+  EXPECT_TRUE(ParseString("foo bar;")) << "Didn't parse successfully";
+  EXPECT_EQ(1, config_.statements_.size()) << "Didn't get one statement";
+  EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0));
+}
+
+TEST_F(NginxStringConfigTest, LotsOfSemicolons){
+
+  EXPECT_FALSE(ParseString(";;;;;;;;;;")) << "lotsa semicolons are ok?";
+
+}
+
+TEST_F(NginxStringConfigTest, WeirdBrackets){
+
+  EXPECT_FALSE(ParseString("thing1 thing2{beep boop;};")) << "weird brackets allowed?";
+
+}
