c0940e57e87d814dfab5061aa7032d6a7c7609ae
Kate <klewi13@gmail.com>
2016-04-08 10:34:52 -0700

    added unit tests, example config files, and fixed bug

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,48 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+#include <sstream>
+#include <string>
 
-  bool success = parser.Parse("example_config", &out_config);
+class NginxStringConfigTest : public ::testing::Test {
+ protected:
+  bool ParseString(const std::string& config_string) {
+    std::stringstream config_stream(config_string);
+    return parser_.Parse(&config_stream, &out_config_);
+  }
+  NginxConfigParser parser_;
+  NginxConfig out_config_;
+};
 
-  EXPECT_TRUE(success);
+TEST_F(NginxStringConfigTest, SimpleConfig) {
+  EXPECT_TRUE(ParseString("foo bar;"));
+  // We can also add expectations about the contents of the statements:
+  EXPECT_EQ(1, out_config_.statements_.size());
+  EXPECT_EQ("foo", out_config_.statements_.at(0)->tokens_.at(0));
 }
+
+TEST_F(NginxStringConfigTest, EmptyConfig) {
+	EXPECT_FALSE(ParseString("")) << "Got empty string wrong";
+}
+
+TEST_F(NginxStringConfigTest, BracketConfig) {
+	EXPECT_TRUE(ParseString("hello world;")) << "Got closed bracket wrong";
+}
+
+TEST(NginxConfigParserTest, ActiveCollabConfig) {
+	NginxConfigParser parser;
+	NginxConfig out_config;
+
+	bool success = parser.Parse("activecollab_config", &out_config);
+	EXPECT_TRUE(success);
+}
+
+
+TEST(NginxConfigParserTest, MoinMoinConfig) {
+	NginxConfigParser parser;
+	NginxConfig out_config;
+
+	bool success = parser.Parse("activecollab_config", &out_config);
+	EXPECT_TRUE(success);
+}
+
