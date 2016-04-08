8d472d947ddb8fcc14c10bf58f24ce87d5e1f2dd
Loko <lokokung@gmail.com>
2016-04-07 17:54:35 -0700

    Implement passing tests for NginxConfigStatement class

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,38 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+/*****************************************************************
+ * Testing Class: NginxConfigStatement
+ * Number of Tests: 2
+ *****************************************************************/
+class NginxConfigStatementTest : public ::testing::Test {
+protected:
+  void pbParent(std::string token) {
+    parentStatement_.tokens_.push_back(token);
+  }
+  void pbChild(std::string token) {
+    childStatement_->tokens_.push_back(token);
+  }
+  void link() {
+    config_->statements_.emplace_back(childStatement_);
+    parentStatement_.child_block_.reset(config_);
+  }
+  NginxConfigStatement parentStatement_;
+  NginxConfigStatement* childStatement_ = new NginxConfigStatement;
+  NginxConfig* config_ = new NginxConfig;
+};
 
-  bool success = parser.Parse("example_config", &out_config);
+TEST_F(NginxConfigStatementTest, ToStringNoChildBlock) {
+  pbParent("foo");
+  pbParent("bar");
+  EXPECT_EQ("foo bar;\n", parentStatement_.ToString(0));
+}
 
-  EXPECT_TRUE(success);
+TEST_F(NginxConfigStatementTest, ToStringChildBlock) {
+  pbParent("server");
+  pbChild("port");
+  pbChild("8000");
+  link();
+  EXPECT_EQ("server {\n  port 8000;\n}\n",
+	    parentStatement_.ToString(0));
 }
