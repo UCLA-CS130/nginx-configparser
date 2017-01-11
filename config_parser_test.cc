5a573c182f20d59065c69b8f76171e0e347a7dee
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-01-11 13:41:27 -0800

    Use test fixture for parser tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,25 @@
-#include "gtest/gtest.h"
+#include <iostream>
 #include "config_parser.h"
+#include "gtest/gtest.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+// Test fixture to hold parser and parsed result
+// Fresh fixture instances are created for every test case
+class NginxConfigParserTest : public ::testing::Test {
+protected:
+    // No SetUp or TearDown required here
+    NginxConfigParser parser;
+    NginxConfig out_config;
+};
 
+TEST_F(NginxConfigParserTest, SimpleConfig) {
   bool success = parser.Parse("example_config", &out_config);
+  std::cerr << out_config.ToString() << std::endl;
 
-  EXPECT_TRUE(success);
+  ASSERT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, EmptyConfig) {
+    bool failure = parser.Parse("", &out_config);
+
+    ASSERT_FALSE(failure);
 }
