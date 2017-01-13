7fffe4a528a0ae26574d2850173239bddd477efb
haochiencho@gmail.com <haochiencho@gmail.com>
2017-01-12 22:06:30 -0800

    fixed bugs with nested config statements and unbalanced curly braces

--- config_parser.cc
+++ config_parser.cc
@@ -152,6 +152,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
   config_stack.push(config);
   TokenType last_token_type = TOKEN_TYPE_START;
   TokenType token_type;
+    std::stack<char> balancedBrace;
   while (true) {
     std::string token;
     token_type = ParseToken(config_file, &token);
@@ -190,6 +191,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         break;
       }
     } else if (token_type == TOKEN_TYPE_START_BLOCK) {
+        balancedBrace.push('{');
       if (last_token_type != TOKEN_TYPE_NORMAL) {
         // Error.
         break;
@@ -199,7 +201,11 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
           new_config);
       config_stack.push(new_config);
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END) {
+        if (balancedBrace.empty())
+            return false;
+        else
+            balancedBrace.pop();
+      if (last_token_type != TOKEN_TYPE_END_BLOCK && last_token_type != TOKEN_TYPE_STATEMENT_END) {
         // Error.
         break;
       }
@@ -210,7 +216,10 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         // Error.
         break;
       }
-      return true;
+        if(balancedBrace.empty())
+            return true;
+        else
+            return false;
     } else {
       // Error. Unknown token.
       break;
