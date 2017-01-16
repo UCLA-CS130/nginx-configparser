3f0d2e2dec5f8ed16f22c76a317ae8af0b32bd3d
haoxiangzhx <haoxiangzhx@gmail.com>
2017-01-15 23:50:27 -0800

    fix missing block and error of last nested block

--- config_parser.cc
+++ config_parser.cc
@@ -152,6 +152,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
   config_stack.push(config);
   TokenType last_token_type = TOKEN_TYPE_START;
   TokenType token_type;
+  int block_count = 0;
   while (true) {
     std::string token;
     token_type = ParseToken(config_file, &token);
@@ -194,19 +195,25 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         // Error.
         break;
       }
+      block_count++;
       NginxConfig* const new_config = new NginxConfig;
       config_stack.top()->statements_.back().get()->child_block_.reset(
           new_config);
       config_stack.push(new_config);
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END) {
+      if (last_token_type != TOKEN_TYPE_STATEMENT_END && last_token_type != TOKEN_TYPE_END_BLOCK) {
         // Error.
         break;
       }
+      block_count--;
       config_stack.pop();
     } else if (token_type == TOKEN_TYPE_EOF) {
       if (last_token_type != TOKEN_TYPE_STATEMENT_END &&
           last_token_type != TOKEN_TYPE_END_BLOCK) {
+        // Error.
+        break;
+      }
+      if(block_count != 0){
         // Error.
         break;
       }
