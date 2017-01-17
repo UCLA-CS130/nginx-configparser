0f30f23e12b59eecd69e1381f58b7f6c0ff3673b
Richard Min <richardmin97@gmail.com>
2017-01-16 16:44:03 -0800

    Allows for embedded blocks

--- config_parser.cc
+++ config_parser.cc
@@ -202,7 +202,8 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
       config_stack.push(new_config);
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
       depth--;
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END) {
+      if (last_token_type != TOKEN_TYPE_STATEMENT_END && 
+          last_token_type != TOKEN_TYPE_END_BLOCK) {
         // Error.
         break;
       }
