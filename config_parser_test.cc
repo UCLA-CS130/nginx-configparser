02135e4a5ca81851eddac459becd1f0aff73b039
Jason Liu <jason.sean.liu@gmail.com>
2017-01-16 17:05:52 -0800

    Added unit tests for config_parser

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -38,11 +38,46 @@ TEST_F(NginxStringConfigTest, AnotherSimpleConfig) {
 	EXPECT_EQ("foo", out_config_.statements_.at(0)->tokens_.at(0));
 }
 
+TEST_F(NginxStringConfigTest, SimplerConfig) {
+	EXPECT_TRUE(ParseString("foo;"));
+}
+
+TEST_F(NginxStringConfigTest, SimplerConfig2) {
+	EXPECT_TRUE(ParseString("boo far; foo {}"));
+}
+
+TEST_F(NginxStringConfigTest, NoConfig) {
+	EXPECT_TRUE(ParseString(""));
+}
+
 TEST_F(NginxStringConfigTest, InvalidConfig) {
 	EXPECT_FALSE(ParseString("foo bar"));
 }
 
 TEST_F(NginxStringConfigTest, NestedConfig) {
 	EXPECT_TRUE(ParseString("server { listen 80; }"));
-	// TODO: Test the contents of out_config_;
+}
+
+TEST_F(NginxStringConfigTest, InvalidNestedConfig) {
+	EXPECT_FALSE(ParseString("server { listen 80; "));
+}
+
+TEST_F(NginxStringConfigTest, MultipleNestedConfig) {
+	EXPECT_TRUE(ParseString("server { foo { listen 80; } }"));
+}
+
+TEST_F(NginxStringConfigTest, MultiNestMultiStatementConfig) {
+	EXPECT_TRUE(ParseString("server { foo { listen 80; } abc def; }"));
+}
+
+TEST_F(NginxStringConfigTest, CommentConfig) {
+	EXPECT_TRUE(ParseString("# nothing to see here"));
+}
+
+TEST_F(NginxStringConfigTest, QuoteConfig) {
+	EXPECT_TRUE(ParseString("foo \"bar\";"));
+}
+
+TEST_F(NginxStringConfigTest, InvalidQuoteConfig) {
+	EXPECT_FALSE(ParseString("foo \"bar;"));
 }
