79b2ace061ff96ba687a7bb9d5a62b5b1fd5511c
Spencer Gervais <spencergervais21@gmail.com>
2017-01-18 21:08:15 +0000

    Fixed bug for nested config blocks, unmatched braces. Added unit tests.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <string>
+#include <sstream>
 
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
@@ -9,3 +11,37 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+class NginxStringConfigTest : public ::testing::Test {
+protected:
+	bool ParseString(const std::string config_string) {
+		std::stringstream config_stream(config_string);
+		return parser_.Parse(&config_stream, &out_config_);
+	}
+	NginxConfigParser parser_;
+	NginxConfig out_config_;
+};
+
+TEST_F(NginxStringConfigTest, InvalidConfig) {
+	EXPECT_FALSE(ParseString("foo bar"));
+}
+
+TEST_F(NginxStringConfigTest, NestedConfigs) {
+	EXPECT_TRUE(ParseString("foo { bar { baz; } }"));
+}
+
+TEST_F(NginxStringConfigTest, ImproperlyNestedConfigs) {
+	EXPECT_FALSE(ParseString("foo { bar { baz; } } }"));
+}
+
+TEST_F(NginxStringConfigTest, BlockLeftUnclosed) {
+	EXPECT_FALSE(ParseString("foo { bar { baz; } "));
+}
+
+TEST_F(NginxStringConfigTest, MultipleTokensBeforeBlock) {
+	EXPECT_TRUE(ParseString("foo bar { baz; }"));
+}
+
+
+
+
