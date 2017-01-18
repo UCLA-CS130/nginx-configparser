0266939b07eefb05492631aaf4fbd966ae2acb33
David Stephan <stephandavid3@gmail.com>
2017-01-18 15:19:49 -0800

    more comprehensive testing

--- config_parser.cc
+++ config_parser.cc
@@ -200,15 +200,15 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
       config_stack.push(new_config);
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
       if (last_token_type != TOKEN_TYPE_STATEMENT_END && 
-		  last_token_type != TOKEN_TYPE_START_BLOCK &&
-		  last_token_type != TOKEN_TYPE_END_BLOCK) {
+      last_token_type != TOKEN_TYPE_START_BLOCK &&
+      last_token_type != TOKEN_TYPE_END_BLOCK) {
         // Error.
         break;
       }
-	  if (config_stack.size() == 1) {
-		  // Error - unexpected }
-		  break;
-	  }
+    if (config_stack.size() == 1) {
+      // Error - unexpected }
+      break;
+    }
       config_stack.pop();
     } else if (token_type == TOKEN_TYPE_EOF) {
       if (last_token_type != TOKEN_TYPE_STATEMENT_END &&
@@ -216,10 +216,10 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         // Error.
         break;
       }
-	  if (config_stack.size() != 1) {
-		  // Error - expected more }
-		  break;
-	  }
+    if (config_stack.size() != 1) {
+      // Error - expected more }
+      break;
+    }
       return true;
     } else {
       // Error. Unknown token.
