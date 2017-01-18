f702b3d229f18d6a30773d2ebaffb0d113714f5f
Brandon <bhaffen97@gmail.com>
2017-01-17 21:00:11 -0800

    Fixed scope operator not working properly in config files

--- config_parser.cc
+++ config_parser.cc
@@ -152,6 +152,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
   config_stack.push(config);
   TokenType last_token_type = TOKEN_TYPE_START;
   TokenType token_type;
+  unsigned int blocks = 0;
   while (true) {
     std::string token;
     token_type = ParseToken(config_file, &token);
@@ -198,15 +199,20 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
       config_stack.top()->statements_.back().get()->child_block_.reset(
           new_config);
       config_stack.push(new_config);
+      blocks = blocks + 1;
     } else if (token_type == TOKEN_TYPE_END_BLOCK) {
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END) {
+      if ((last_token_type != TOKEN_TYPE_STATEMENT_END &&
+           last_token_type != TOKEN_TYPE_END_BLOCK) ||
+          blocks < 1) {
         // Error.
         break;
       }
       config_stack.pop();
+      blocks = blocks - 1;
     } else if (token_type == TOKEN_TYPE_EOF) {
-      if (last_token_type != TOKEN_TYPE_STATEMENT_END &&
-          last_token_type != TOKEN_TYPE_END_BLOCK) {
+      if ((last_token_type != TOKEN_TYPE_STATEMENT_END &&
+           last_token_type != TOKEN_TYPE_END_BLOCK) ||
+          blocks > 0) {
         // Error.
         break;
       }
--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,61 @@
+#include <sstream>
+#include <string>
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
+
+class NginxConfigParserTest : public ::testing::Test
+{
+protected:
+  
+  bool ParseString(const std::string& str)
+  {
+    out_config.statements_.clear();
+    std::stringstream ss(str);
+    return parser.Parse(&ss, &out_config);
+  }
+  
   NginxConfigParser parser;
   NginxConfig out_config;
+};
 
-  bool success = parser.Parse("example_config", &out_config);
 
-  EXPECT_TRUE(success);
+TEST_F(NginxConfigParserTest, TokenTest) {
+  EXPECT_TRUE(ParseString("foo;"));
+  EXPECT_EQ("foo;\n", out_config.ToString());
+  EXPECT_TRUE(ParseString("foo \"bar baz\"\t'bar baz'; # Comment"));
+  EXPECT_EQ("foo \"bar baz\" 'bar baz';\n", out_config.ToString());
+  EXPECT_FALSE(ParseString("foo"));
+  EXPECT_FALSE(ParseString("foo bar"));
+  EXPECT_FALSE(ParseString(""));
+  EXPECT_FALSE(ParseString(" "));
+  EXPECT_FALSE(ParseString("  "));
 }
+
+
+TEST_F(NginxConfigParserTest, ScopeTest) {
+  EXPECT_TRUE(ParseString("foo \"bar\" { baz; }"));
+  EXPECT_EQ("foo \"bar\" {\n  baz;\n}\n", out_config.ToString(0));
+  EXPECT_TRUE(ParseString("foo { \"bar\" { baz; } }"));
+  EXPECT_EQ("foo {\n  \"bar\" {\n    baz;\n  }\n}\n", out_config.ToString(0));
+  EXPECT_FALSE(ParseString("foo \"bar\" baz; }"));
+  EXPECT_FALSE(ParseString("foo \"bar\" { baz;"));
+  EXPECT_FALSE(ParseString("foo { \"bar\" baz; } }"));
+  EXPECT_FALSE(ParseString("foo \"bar\" { baz; } }"));
+  EXPECT_FALSE(ParseString("foo \"bar\" baz; } }"));
+  EXPECT_FALSE(ParseString("foo { \"bar\" { baz; }"));
+  EXPECT_FALSE(ParseString("foo { \"bar\" { baz;"));
+  EXPECT_FALSE(ParseString("{"));
+  EXPECT_FALSE(ParseString("}"));
+  EXPECT_FALSE(ParseString("{}"));
+  EXPECT_FALSE(ParseString("{ }"));
+  EXPECT_FALSE(ParseString("foo { }"));
+}
+
+
+TEST_F(NginxConfigParserTest, ExampleConfig) {
+  EXPECT_TRUE(parser.Parse("example_config", &out_config));
+  EXPECT_EQ("foo", out_config.statements_.at(0)->tokens_.at(0));
+  EXPECT_FALSE(parser.Parse("null_config", &out_config));
+}
+
